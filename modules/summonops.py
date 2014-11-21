975261fb84eeed80bf5102b9c2e338b53d8cb043
Ivan Uvarov <flashcactus@ya.ru>
2014-11-21 16:03:13 +0300

    Added 2-person safeguard for summonops command

--- modules/summonops.py
+++ modules/summonops.py
@@ -1,6 +1,9 @@
-
+import time
 import willie
 
+def setup(bot):
+	bot.memory['summonops_delay'] = {}
+	bot.memory['summonops_interv'] = 15
 
 @willie.module.commands('ops','summonops')
 def summon_ops(bot, trigger):
@@ -9,6 +12,19 @@ def summon_ops(bot, trigger):
 	THIS COMMAND SHOULD ONLY BE USED IN EMERGENCIES IF YOU DON'T WANT TO BE SMOTE FOR SPAMMING.
 	"""
 	chan = trigger.sender.strip()
-	ops = bot.ops.get(chan, [])
-	bot.say("Hailing all ops! " + ", ".join(ops) + "! " + trigger.nick.strip() + " requests your attention." )
+	nick = trigger.nick.strip()
+	if chan == nick:
+		bot.say("This command is pointless in private.")	
+		return
+	ctime = time.time()
+	dtuple = bot.memory['summonops_delay'].get(chan, (0, nick))
+	if (ctime - dtuple[0]) <= bot.memory['summonops_interv']: # not a confirmation
+		if dtuple[1] == nick:
+			bot.reply("An other person must confirm this, not you.")
+		else:
+			ops = bot.ops.get(chan, [])
+			bot.say("Hailing all ops! " + ", ".join(ops) + "! " + trigger.nick.strip() + " requests your attention." )
+	else:
+		bot.reply("Are you sure? If this truly is an emergency, then another person on the channel must type this command within the next " + str(bot.memory['summonops_interv']) + " seconds." )
+		bot.memory['summonops_delay'][chan] = (time.time(), nick)
 
