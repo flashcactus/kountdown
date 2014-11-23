8f64fe632ddbc45dbfa296df36a92e5eba9f3545
Ivan Uvarov <flashcactus@ya.ru>
2014-11-23 22:35:44 +0300

    Added prefixcactus trigger to cactrack 	modified:   reactions.py

--- modules/reactions.py
+++ modules/reactions.py
@@ -17,5 +17,12 @@ citations = [
 @willie.module.rule('(?:.*[^A-Za-z])?[Cc]itation needed')
 #@willie.module.commands('citation', 'cite')
 def citation(bot, trigger):
+	"Replies with a smartypants link whenever someone requests a citation"
 	bot.reply(random.choice(citations))
+
+
+@willie.module.rule('^\x01ACTION +[Ss]tare.*$')
+#@willie.module.rule('^.*[KkCc]ountdown.*$')
+def stare_back(bot,trigger):
+        bot.msg(trigger.sender,"\x01ACTION stares at "+trigger.nick+"\x01")
 
