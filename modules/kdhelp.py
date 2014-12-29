0072ae6159a4a0fb5e7341f18dabe2f2ccf39c45
Ivan Uvarov <flashcactus@ya.ru>
2014-12-29 15:37:54 +0000

    kdhelp: additional line is now NOTICE'd personally

--- modules/kdhelp.py
+++ modules/kdhelp.py
@@ -4,4 +4,4 @@ import help
 @willie.module.rule('$nick' r'(?i)help(?:[?!]+)?$')
 @willie.module.priority('low')
 def kdhelp(bot, trigger):
-	bot.reply('For more info about this particular bot visit the page at https://github.com/flashcactus/kountdown', notice=True)
+	bot.notice('For more info about this particular bot visit the page at https://github.com/flashcactus/kountdown', trigger.nick.strip())
